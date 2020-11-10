12f9548ad7e2ec0453eb7ab2ee6bdd7ddfa815b0
UsingCoding <extendedmoment@outlook.com>
2020-11-10 23:03:53 +0300

    Вынес исключение апишек в абстрактный класс

---
+++ src/Common/Api/Exception/AbstractApiException.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Common\Api\Exception;
+
+use App\Common\Domain\Utils\Arrays;
+use App\Module\User\Api\Exception\ApiException;
+
+abstract class AbstractApiException extends \Exception
+{
+    public const UNKNOWN_ERROR = 0;
+
+    private int $type;
+
+    private function __construct(int $type, string $message, int $code, \Throwable $previous)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->type = $type;
+    }
+
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    public static function from(\Throwable $throwable): ApiException
+    {
+        $type = Arrays::get(static::getExceptionMap(), get_class($throwable), self::UNKNOWN_ERROR);
+
+        $exceptionClass = static::getSelf();
+
+        return new $exceptionClass($type, $throwable->getMessage(), $throwable->getCode(), $throwable);
+    }
+
+    abstract protected static function getSelf(): string;
+
+    abstract protected static function getExceptionMap(): array;
+}
--- src/Module/User/Api/Exception/ApiException.php
+++ src/Module/User/Api/Exception/ApiException.php
@@ -3,39 +3,25 @@
 
 namespace App\Module\User\Api\Exception;
 
-use App\Common\Domain\Utils\Arrays;
+use App\Common\Api\Exception\AbstractApiException;
 use App\Module\User\App\Exception\IncorrectUserPasswordException;
 use App\Module\User\App\Exception\UserNotFoundException;
 
-class ApiException extends \Exception
+class ApiException extends AbstractApiException
 {
-    public const UNKNOWN_ERROR = 0;
     public const USER_NOT_FOUND = 1;
     public const INCORRECT_PASSWORD = 2;
 
-    private const EXCEPTION_CLASS_CODE_MAP = [
-        UserNotFoundException::class => self::USER_NOT_FOUND,
-        IncorrectUserPasswordException::class => self::INCORRECT_PASSWORD
-    ];
-
-    private int $type;
-
-    private function __construct(int $type, string $message, int $code, \Throwable $previous)
+    protected static function getSelf(): string
     {
-        parent::__construct($message, $code, $previous);
-
-        $this->type = $type;
+        return __CLASS__;
     }
 
-    public function getType(): int
+    protected static function getExceptionMap(): array
     {
-        return $this->type;
-    }
-
-    public static function from(\Throwable $throwable): self
-    {
-        $type = Arrays::get(self::EXCEPTION_CLASS_CODE_MAP, get_class($throwable), self::UNKNOWN_ERROR);
-
-        return new self($type, $throwable->getMessage(), $throwable->getCode(), $throwable);
+        return [
+            UserNotFoundException::class => self::USER_NOT_FOUND,
+            IncorrectUserPasswordException::class => self::INCORRECT_PASSWORD
+        ];
     }
 }
