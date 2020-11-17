b07c52ccfa32fa94b7ea0d924f322bbdaefd33f7
UsingCoding <extendedmoment@outlook.com>
2020-11-18 00:01:41 +0300

    Добавил миграцию для проекта

---
+++ src/Migrations/Account/Version20201111192603.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AccountDoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201111192603 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE project (project_id SERIAL NOT NULL, name VARCHAR(50) NOT NULL, name_id VARCHAR(10) NOT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(project_id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_2FB3D0EE71179CD6 ON project (name_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE project');
+    }
+}
