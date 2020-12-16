9ddc63ffe14cd52937b846199be83ffb60f8424b
UsingCoding <extendedmoment@outlook.com>
2020-12-16 11:21:59 +0300

    Упростил работу перехватчика исключений

--- src/Framework/Infrastructure/Listener/ExceptionHandlerListener.php
+++ src/Framework/Infrastructure/Listener/ExceptionHandlerListener.php
@@ -2,8 +2,6 @@
 
 namespace App\Framework\Infrastructure\Listener;
 
-use App\Common\App\Exception\AbstractContextException;
-use App\Common\Domain\Utils\Arrays;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -29,33 +27,7 @@ class ExceptionHandlerListener implements EventSubscriberInterface
             return;
         }
 
-        $exception = $event->getThrowable();
-
-        $contextException = $this->findContextException($exception);
-
-        if ($contextException === null)
-        {
-            return;
-        }
-
-        $event->setResponse(new JsonResponse(Arrays::merge(['error' => 'internal'], $contextException->getContext()), 500));
-    }
-
-    private function findContextException(\Throwable $throwable): ?AbstractContextException
-    {
-        $previous = $throwable->getPrevious();
-
-        if ($previous === null)
-        {
-            return null;
-        }
-
-        if ($previous instanceof AbstractContextException && $previous->getContext() !== null)
-        {
-            return $previous;
-        }
-
-        return $this->findContextException($previous);
+        $event->setResponse(new JsonResponse(['error' => 'internal'], 500));
     }
 
     public static function getSubscribedEvents(): array
