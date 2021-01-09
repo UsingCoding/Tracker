ec0af38f4221d044e45618061ce4b2e6cb14610b
temagavr <temagavr22@gmail.com>
2021-01-09 20:56:12 +0300

    Сделан поиск при нажатии на enter

--- assets/js/components/SearchPanel.vue
+++ assets/js/components/SearchPanel.vue
@@ -4,11 +4,9 @@
             <span class="search_type" >Everything</span>
         </div>
         <div class="search_field">
-            <!-- <div class="search_input" spellcheck="false" tabindex="0" contenteditable="true" role="textbox">
-            </div> -->
-            <input v-model="search" class="search_input" type="text"/>
+            <input v-model="search" v-on:keyup.enter="findIssues()" class="search_input" type="text" id="search_str"/>
             <div class="">
-                <span v-on:click="find()" class="search_button">Search</span>
+                <span v-on:click="findIssues()" class="search_button">Search</span>
             </div>
         </div>
     </div>
@@ -22,7 +20,7 @@ export default {
         }
     },
     methods: {
-        find: function () {
+        findIssues: function () {
             if(this.$route.name=="issues")
             {
                 this.$emit('find', this.search);
