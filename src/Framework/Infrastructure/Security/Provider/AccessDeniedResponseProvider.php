5f75fc028610761f4223926c34c87a14f3b2318a
UsingCoding <extendedmoment@outlook.com>
2021-01-11 00:59:35 +0300

    Добавил редирект при переходе будучи незологиненным на запрещенный урл

--- src/Framework/Infrastructure/Security/Provider/AccessDeniedResponseProvider.php
+++ src/Framework/Infrastructure/Security/Provider/AccessDeniedResponseProvider.php
@@ -2,13 +2,23 @@
 
 namespace App\Framework\Infrastructure\Security\Provider;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AccessDeniedResponseProvider implements AccessDeniedResponseProviderInterface
 {
+    private const LOGIN_ROUTE = 'frontend_login';
+
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
     public function getResponse(): Response
     {
-        return new JsonResponse(['error' => 'access_denied'], 403);
+        return new RedirectResponse($this->urlGenerator->generate(self::LOGIN_ROUTE));
     }
 }
