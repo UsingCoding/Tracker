8a4b86fee77a11e92b265f96b92ec2e0038b6208
temagavr <temagavr22@gmail.com>
2021-01-11 01:41:45 +0300

    Добавлены тайтлы к страницам, поправлена верстка

--- assets/js/Router.js
+++ assets/js/Router.js
@@ -16,23 +16,37 @@ import access_denied from "./pages/AccessDeniedPage";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+let vueRouter = new VueRouter({
     mode: 'history',
     routes:[
-        { path:'/login', name: 'login', component: login },
-        { path:'/issues', name:'issues', component: issues },
-        { path:'/create-issue', name:'create_issue', component: create_issue },
-        { path:'/issue/:code', name:'issue_details', component: issue_details },
-        { path:'/projects', name:'projects_list', component: projects_list },
-        { path:'/create-project', name:'create_project', component: create_project },
-        { path:'/project/:code', name:'project_info', component: project_info },
-        { path: '/project/:code/fields', name: 'project_fields', component: project_fields},
-        { path: '/project/:code/settings', name: 'project_settings', component: project_settings},
-        { path: '/project/:code/team', name: 'project_team', component: project_team},
-        { path: '/user/:code', name: 'user_info', component: user_info},
-        { path: '/add/user', name: 'create_user', component: user_info},
-        { path: '/users', name: 'users_list', component: users_list},
-        { path: '/404', name: 'access_denied', component: access_denied },
+        { path:'/login', name: 'login', component: login, meta: { title: 'Login' }  },
+        { path:'/issues', name:'issues', component: issues, meta: { title: 'Issues' } },
+        { path:'/create-issue', name:'create_issue', component: create_issue, meta: { title: 'New Issue' } },
+        { path:'/issue/:code', name:'issue_details', component: issue_details, meta: { title: 'Issue' } },
+        { path:'/projects', name:'projects_list', component: projects_list, meta: { title: 'Projects' } },
+        { path:'/create-project', name:'create_project', component: create_project, meta: { title: 'New Project' } },
+        { path:'/project/:code', name:'project_info', component: project_info, meta: { title: 'Project' } },
+        { path: '/project/:code/fields', name: 'project_fields', component: project_fields, meta: { title: 'Project Fields' }},
+        { path: '/project/:code/settings', name: 'project_settings', component: project_settings, meta: { title: 'Project Settings' }},
+        { path: '/project/:code/team', name: 'project_team', component: project_team, meta: { title: 'Project Team' }},
+        { path: '/user/:code', name: 'user_info', component: user_info, meta: { title: 'User Info' }},
+        { path: '/add/user', name: 'create_user', component: user_info, meta: { title: 'Create User' }},
+        { path: '/users', name: 'users_list', component: users_list, meta: { title: 'Users' }},
+        { path: '/404', name: 'access_denied', component: access_denied, meta: { title: 'Get Lost?' } },
         { path: '*', redirect: '/404' }
     ]
 });
+
+vueRouter.beforeEach(( to, from, next) => {
+    document.title = to.meta.title
+
+    next()
+});
+
+export default vueRouter;
+
+// VueRouter.afterEach(( to, from) => {
+//     Vue.nextTick(() => {
+//         document.title = to.meta.title;
+//     });
+// });
